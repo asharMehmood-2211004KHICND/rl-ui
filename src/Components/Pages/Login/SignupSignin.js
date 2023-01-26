81c22f437ab23d0a214db44417445f3ef92dadb1
afnanyousuf89 <afnan.yousuf@xloopdigital.com>
2023-01-26 09:16:11 +0500

    api url

--- src/Components/Pages/Login/SignupSignin.js
+++ src/Components/Pages/Login/SignupSignin.js
@@ -94,7 +94,7 @@ function SignupSignin() {
 				email: login_email,
 				password: login_password
 			}
-			fetch("https://54.234.80.15:8080/auth/login", {
+			fetch("http://54.234.80.15:8080/auth/login", {
 				method: 'POST',
 				body: JSON.stringify(data),
 				headers: {
@@ -194,7 +194,7 @@ function SignupSignin() {
 					id: 1
 				}
 			}
-			fetch("https://54.234.80.15:8080/auth/register", {
+			fetch("http://54.234.80.15:8080/auth/register", {
 				method: 'POST',
 				body: JSON.stringify(data),
 				headers: {
