856105bc1cd6212bbc41278a86261ea7050e4e15
hunainparekh2211006KHICND <hunain.parekh@xloopdigital.com>
2023-02-06 11:11:22 +0500

    some chnages in staff creation page

--- src/Components/Pages/Staff/CreateStaff.js
+++ src/Components/Pages/Staff/CreateStaff.js
@@ -4,33 +4,75 @@ import DropdownField from "../profile/DropdownField/DropdownField";
 import InputField from "../profile/InputField/InputField";
 import styles from "./CreateStaff.module.css";
 import Male from "../../images/male.png";
-import { AlertMessage } from "../profile/AlertMessage/AlertMessage.js";
+import swal from 'sweetalert';
 
 
 const CreateStaff = () => {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [first_name, setfirst_name] = useState('');
+    const [last_name, setlast_name] = useState('');
     const [email, setEmail] = useState('');
     const [nic, setNic] = useState('');
     const [gender, setGender] = useState('');
     const [dob, setDob] = useState('');
     const [password, setPassword] = useState('');
     const [designation, setDesignation] = useState('');
-    const [roles, setRoles] = useState('');
-
-    const [showAlert, setShowAlert] = useState(false)
-    const [alertType, setalertType] = useState('alert');
-    const [message, setMessage] = useState('')
+    const [role, setRole] = useState('Hello');
+    const [errors, setErrors] = useState({});
 
 
+    const validate = (values) => {
+        let errors = {};
+        if (!values.email) {
+            errors.email = "Email address is required";
+        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+            errors.email = "Email address is invalid";
+        } else if (/^\d/.test(values.email)) {
+            errors.email = "Email should not contain number in start";
+        }
+        if (!values.password) {
+            errors.password = "Password is required";
+        } else if (values.password.length < 8) {
+            errors.password = "Password must be 8 or more characters";
+        } else if (!/\d/.test(values.password)) {
+            errors.password = "Password must contain atleast 1 number";
+        } else if (!/[!@#$%&?]/g.test(values.password)) {
+            errors.password = "Password must contain atleast 1 special character";
+        } else if (!/[A-Z]/g.test(values.password)) {
+            errors.password = "Password must contain atleast 1 capital letter";
+        }
+        if (!values.first_name) {
+            errors.first_name = "First Name is required";
+        }
+        else if (/[!@#$%&?]/g.test(values.first_name) || /\d/.test(values.first_name)) {
+            errors.first_name = "First Name should not contain numbers or any special character";
+        }
+        if (values.last_name) {
+            if (/[!@#$%&?]/g.test(values.last_name) || /\d/.test(values.last_name)) {
+                errors.last_name = "Last Name should not contain numbers or any special character";
+            }
+        }
+        return errors;
+    }
 
-    const handleFirstname = useCallback(val => {
-        setFirstName(val);
+    const roleOptions = [
+        {
+            "label": "Interviewer",
+            "value": 2
+        },
+        {
+            "label": "Hiring Manager",
+            "value": 3
+        }
+    ]
+
+
+    const handlefirst_name = useCallback(val => {
+        setfirst_name(val);
     }, []);
 
-    const handleLastname = useCallback(val => {
-        setLastName(val);
+    const handlelast_name = useCallback(val => {
+        setlast_name(val);
     }, []);
 
     const handleEmail = useCallback(val => {
@@ -57,8 +99,8 @@ const CreateStaff = () => {
         setDesignation(val);
     }, []);
 
-    const handleRoles = useCallback(val => {
-        setRoles(val);
+    const handleRole = useCallback(e => {
+        setRole(e.target.value);
     }, []);
 
     const url = "http://localhost:8080/creatstaff"
@@ -66,77 +108,100 @@ const CreateStaff = () => {
     async function onSubmit(event) {
         event.preventDefault();
 
+        const errors = validate({ email, password, first_name, last_name });
+        setErrors(errors);
+
         const data = {
-            firstName: firstName,
-            lastName: lastName,
+            first_name: first_name,
+            last_name: last_name,
             email: email,
             cnic: nic,
             gender: gender,
             date_of_birth: dob,
             password: password,
             designation: designation,
-            role: roles
+            role: role
         }
 
-        console.log(data)
-        fetch(url, {
+        const signupData = {
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password,
+            role: {
+                id: role
+            }
+        }
+
+        fetch("http://authenticationserviceelastic-env.eba-pf8t7rhm.us-east-1.elasticbeanstalk.com/auth/register", {
             method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
+            body: JSON.stringify(signupData),
             headers: {
-                'Content-Type': 'application/json',
+                'Content-type': 'application/json; charset=UTF-8',
             },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
-            body: JSON.stringify(data)
-        })
-            .then(response => {
-                console.log(response);
-                const res = response ? response.ok : false;
-                const updateUser = res ? 'Info saved successfully!' : 'Error saving info!';
-                const alertUser = res ? 'info' : 'danger';
-                setShowAlert(true);
-                setMessage(updateUser)
-                setalertType(alertUser)
-                setTimeout(() => {
-                    setShowAlert(false)
-                }, "2000")
-
-                // swal({
-                //     title: "Personal Information Saved!",
-                //     icon: "success",
-                // })
-            })
-            .catch(err => {
-                console.log(err)
-                const updateUser = 'Error saving info!';
-                const alertUser = 'danger';
-                setShowAlert(true);
-                setMessage(updateUser)
-                setalertType(alertUser)
-                setTimeout(() => {
-                    setShowAlert(false)
-                }, "2000")
-            });
+        }).then((response) => {
+            if (response.status == 200) {
+                fetch(url, {
+                    method: 'POST',
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    redirect: 'follow',
+                    referrerPolicy: 'no-referrer',
+                    body: JSON.stringify(data)
+                })
+                    .then(response => {
+                        console.log(response);
+                        swal({
+                            title: "User Created Successfully!",
+                            icon: "success",
+                            buttons: {
+                                confirm: { text: 'Login', className: 'btn' },
+                            },
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        swal({
+                            title: "Error saving info!",
+                            icon: "error",
+                        }
+                        );
+                    });
+            }
+            else if (response.status == 409) {
+                swal(
+                    {
+                        title: "This Email Already Registered!",
+                        icon: "warning",
+                    });
+            }
+            else if (response.status == 404) {
+                swal({
+                    title: "Server Not Responding!",
+                    icon: "error",
+                }
+                );
+            }
+        }
+        );
     }
-    
-
-
     return (
         <>
             <div className={styles.mainContainer}>
                 <form className={styles.formCreateStaff} onSubmit={onSubmit}>
-                    {showAlert ? <AlertMessage showAlert={showAlert} setAlert={setShowAlert} alertType={alertType} message={message} /> : ''}
                     <h1 className={styles.createStaffHeading}>Create User</h1>
                     <table className={styles.table}>
                         <tr>
                             <td ><label >First Name:</label></td>
-                            <td colSpan="3"><InputField value={firstName} handler={handleFirstname} type='text' placeholder='First Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required' icon='fa-solid fa-user'></InputField></td>
+                            <td colSpan="3"><InputField value={first_name} handler={handlefirst_name} type='text' placeholder='First Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required' icon='fa-solid fa-user'></InputField></td>
                             <td></td>
                             <td ><label>Last Name: </label></td>
                             <td></td>
-                            <td colSpan=""><InputField value={lastName} handler={handleLastname} type='text' placeholder='Last Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required'></InputField></td>
+                            <td colSpan=""><InputField value={last_name} handler={handlelast_name} type='text' placeholder='Last Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required'></InputField></td>
                         </tr>
                         <tr>
                             <td><label className={styles.label}>Email:</label></td>
@@ -169,9 +234,14 @@ const CreateStaff = () => {
                             <td></td>
                             <td><label className={styles.label}>Roles: </label></td>
                             <td></td>
-                            <td><DropdownField value={roles} handler={handleRoles} options={['Hiring Manager', 'Interviewer']} placeholder='Roles' className={styles.halfSize} required='required' icon='fa-solid fa-tasks'></DropdownField>
-                            </td>
-                            
+                            {/* <td><DropdownField value={roles} handler={handleRoles} options={['Interviewer','Hiring Manager']} placeholder='Roles' className={styles.halfSize} required='required' icon='fa-solid fa-tasks'></DropdownField>
+                            </td> */}
+                            <td><select value={role} onChange={handleRole} className={styles.halfSize}>
+                                {roleOptions.map((option) => (
+                                    <option value={option.value}>{option.label}</option>
+                                ))}
+                            </select></td>
+
                         </tr>
                     </table>
                     <div className={styles.buttonContainerStaff}>
