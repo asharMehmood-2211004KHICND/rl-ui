23e5822d58687ce55360b46193fec79bc9e8236d
Zia_Rasheed_2211017KHICND <119280210+ZiaRasheedAhmed@users.noreply.github.com>
2023-02-06 09:53:23 +0500

    Create Staff UI

--- src/Components/Pages/Staff/CreateStaff.js
+++ src/Components/Pages/Staff/CreateStaff.js
@@ -67,15 +67,15 @@ const CreateStaff = () => {
         event.preventDefault();
 
         const data = {
-            firstName : firstName,
-            lastName : lastName,
-            email : email,
-            cnic : nic,
-            gender : gender,
-            date_of_birth : dob,
-            password : password,
-            designation : designation,
-            role : roles
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            cnic: nic,
+            gender: gender,
+            date_of_birth: dob,
+            password: password,
+            designation: designation,
+            role: roles
         }
 
         console.log(data)
@@ -91,36 +91,36 @@ const CreateStaff = () => {
             referrerPolicy: 'no-referrer',
             body: JSON.stringify(data)
         })
-        .then(response => {
-            console.log(response);
-            const res = response ? response.ok : false;
-            const updateUser = res ? 'Info saved successfully!' : 'Error saving info!';
-            const alertUser = res ? 'info' : 'danger';
-            setShowAlert(true);
-            setMessage(updateUser)
-            setalertType(alertUser)
-            setTimeout(() => {
-                setShowAlert(false)
-            }, "2000")
+            .then(response => {
+                console.log(response);
+                const res = response ? response.ok : false;
+                const updateUser = res ? 'Info saved successfully!' : 'Error saving info!';
+                const alertUser = res ? 'info' : 'danger';
+                setShowAlert(true);
+                setMessage(updateUser)
+                setalertType(alertUser)
+                setTimeout(() => {
+                    setShowAlert(false)
+                }, "2000")
 
-            // swal({
-            //     title: "Personal Information Saved!",
-            //     icon: "success",
-            // })
-        })
-        .catch(err => {
-            console.log(err)
-            const updateUser = 'Error saving info!';
-            const alertUser = 'danger';
-            setShowAlert(true);
-            setMessage(updateUser)
-            setalertType(alertUser)
-            setTimeout(() => {
-                setShowAlert(false)
-            }, "2000")
-        });
+                // swal({
+                //     title: "Personal Information Saved!",
+                //     icon: "success",
+                // })
+            })
+            .catch(err => {
+                console.log(err)
+                const updateUser = 'Error saving info!';
+                const alertUser = 'danger';
+                setShowAlert(true);
+                setMessage(updateUser)
+                setalertType(alertUser)
+                setTimeout(() => {
+                    setShowAlert(false)
+                }, "2000")
+            });
     }
-
+    
 
 
     return (
@@ -129,39 +129,53 @@ const CreateStaff = () => {
                 <form className={styles.formCreateStaff} onSubmit={onSubmit}>
                     {showAlert ? <AlertMessage showAlert={showAlert} setAlert={setShowAlert} alertType={alertType} message={message} /> : ''}
                     <h1 className={styles.createStaffHeading}>Create User</h1>
-                    <div>
-                        <InputField value={firstName} handler={handleFirstname} type='text' placeholder='First Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required' icon='fa-solid fa-user'></InputField>
-                        <InputField value={lastName} handler={handleLastname} type='text' placeholder='Last Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required' icon='fa-solid fa-user'></InputField>
-                    </div>
-                    <div className={styles.form_with_pic}>
-                        <div className={styles.feilds}>
-                            <div>
-                                <InputField value={email} handler={handleEmail} type='text' placeholder='Email' className={styles.maxSize} required='required' icon='fa-solid fa-envalope'></InputField>
-                            </div>
-                            <div>
-                                <InputField value={nic} handler={handleNic} type='text' pattern="[0-9]*" placeholder='CNIC/Nation ID' className={styles.maxSize} required='required' icon='fa-solid fa-address-card'></InputField>
-                            </div>
-                            <div>
-                                <InputField value={password} handler={handlePassword} type='password' placeholder='Password' className={styles.maxSize} required='required' icon='fa-solid fa-key'></InputField>
-                            </div>
-                        </div>
-                        <div>
-                            <img className={styles.usericon} src={Male} />
-                        </div>
-                    </div>
+                    <table className={styles.table}>
+                        <tr>
+                            <td ><label >First Name:</label></td>
+                            <td colSpan="3"><InputField value={firstName} handler={handleFirstname} type='text' placeholder='First Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required' icon='fa-solid fa-user'></InputField></td>
+                            <td></td>
+                            <td ><label>Last Name: </label></td>
+                            <td></td>
+                            <td colSpan=""><InputField value={lastName} handler={handleLastname} type='text' placeholder='Last Name' pattern='[a-zA-Z]*' className={styles.halfSize} required='required'></InputField></td>
+                        </tr>
+                        <tr>
+                            <td><label className={styles.label}>Email:</label></td>
+                            <td colSpan="5"><InputField value={email} handler={handleEmail} type='text' placeholder='Email' className={styles.maxSize} required='required' icon='fa-solid fa-envalope'></InputField></td>
+                            <td></td>
+                            <td rowSpan="3"><img className={styles.usericon} src={Male} /></td>
 
+                        </tr>
+                        <tr>
+                            <td><label className={styles.label}>CNIC/ID:</label></td>
+                            <td colSpan="5"><InputField value={nic} handler={handleNic} type='text' pattern="[0-9]*" placeholder='CNIC/Nation ID' className={styles.maxSize} required='required' icon='fa-solid fa-address-card'></InputField></td>
+                        </tr>
+                        <tr>
+                            <td><label className={styles.label}>Password:</label></td>
+                            <td colSpan="5"><InputField value={password} handler={handlePassword} type='password' placeholder='Password' className={styles.maxSize} required='required' icon='fa-solid fa-key'></InputField>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label className={styles.label}>Gender: </label></td>
+                            <td colSpan="3"><DropdownField handler={handleGender} value={gender} options={['Male', 'Female']} className={styles.halfSize} placeholder='Gender' icon='fa-sharp fa-solid fa-person-dress' /></td>
+                            <td></td>
+                            <td><label className={styles.label}>Date of Birth: </label></td>
+                            <td></td>
+                            <td><InputField value={dob} handler={handleDob} type='date' placeholder='Date of Birth' className={styles.halfSize} required='required' icon='fa-solid fa-calendar-days'></InputField></td>
 
-                    <div>
-                        <DropdownField handler={handleGender} value={gender} options={['Male', 'Female']} className={styles.halfSize} placeholder='Gender' icon='fa-sharp fa-solid fa-person-dress' />
-                        <InputField value={dob} handler={handleDob} min="1970-01-01" type='date' placeholder='Date of Birth' className={styles.halfSize} required='required' icon='fa-solid fa-calendar-days'></InputField>
-                    </div>
-
-                    <div>
-                        <DropdownField value={designation} handler={handleDesignation} options={['Manager', 'Trainer', 'Developer', 'Data Engineer']} placeholder='Designation' className={styles.halfSize} required='required' icon='fa-solid fa-level-up'></DropdownField>
-                        <DropdownField value={roles} handler={handleRoles} options={['1', '2', '3']} placeholder='Roles' className={styles.halfSize} required='required' icon='fa-solid fa-tasks'></DropdownField>
-                    </div>
-                    <div className={styles.buttonContainer}>
-                        <Button type="submit" text="Save" className={styles.saveButton}>Submit</Button>
+                        </tr>
+                        <tr>
+                            <td><label className={styles.label}>Designation: </label></td>
+                            <td colSpan="3"><DropdownField value={designation} handler={handleDesignation} options={['Manager', 'Trainer', 'Developer', 'Data Engineer']} placeholder='Designation' className={styles.halfSize} required='required' icon='fa-solid fa-level-up'></DropdownField></td>
+                            <td></td>
+                            <td><label className={styles.label}>Roles: </label></td>
+                            <td></td>
+                            <td><DropdownField value={roles} handler={handleRoles} options={['Hiring Manager', 'Interviewer']} placeholder='Roles' className={styles.halfSize} required='required' icon='fa-solid fa-tasks'></DropdownField>
+                            </td>
+                            
+                        </tr>
+                    </table>
+                    <div className={styles.buttonContainerStaff}>
+                        <Button type="submit" text="Save" className={styles.saveButtonStaff}>Submit</Button>
                     </div>
                 </form>
 
